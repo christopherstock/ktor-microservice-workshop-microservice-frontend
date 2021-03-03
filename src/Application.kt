c2f1a58d85a1d2d6c68ea4e995d8c6d7dd9cc7bf
Christopher Stock <christopher.stock@mayflower.de>
2021-03-03 08:24:45 +0100

    Add HTML DSL ktor lib.

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,8 @@
 package de.mayflower
 
 import io.ktor.application.*
+import io.ktor.html.*
+import kotlinx.html.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -13,6 +15,21 @@ fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
             call.respondText("Hello Frontend!", contentType = ContentType.Text.Plain)
+        }
+        get("/html") {
+            val title = "Frontend HTML Test Route"
+            val acclaim = "Welcome to our HTML Route"
+            val content = "This route returns a rendered HTML body that contains templating."
+
+            call.respondHtml {
+                head {
+                    title { +title }
+                }
+                body {
+                    h1 { +acclaim }
+                    p { +content }
+                }
+            }
         }
     }
 }
